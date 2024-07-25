037f8d1f293b7fcaaf0d5a7f16b0cb9360696a63
andre <evaristoandre106@gmail.com>
2024-07-25 19:41:53 -0300

    colocando a funcionalidade de escolher tipo de cadastro e mais resposividade

--- back-end/cadastro.php
+++ back-end/cadastro.php
@@ -4,9 +4,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $conn->real_escape_string($_POST['nome']);
     $documento = $conn->real_escape_string($_POST['documento']);
     $apartamento = (int) $_POST['apartamento'];
+    $tipoCadastro = $_POST['tipoCadastro'];
+
+    if ($tipoCadastro == 'morador') {
+        $tabela = "moradores";
+    } else {
+        $tabela = "visitantes";
+    }
     
     // SQL para inserir os dados
-    $sql = "INSERT INTO visitantes (nome, documento, apartamento) VALUES ('$nome', '$documento', '$apartamento')";
+    $sql = "INSERT INTO $tabela (nome, documento, apartamento) VALUES ('$nome', '$documento', '$apartamento')";
        
     if ($conn->query($sql) === TRUE) {
         header("Location: ../front-end/confirmacao_de_cadastro.html");
@@ -16,6 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 }
-
-// Fechando a conexão
 $conn->close();
+?>
+
+
